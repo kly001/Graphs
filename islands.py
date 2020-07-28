c1b343a1d27a9e6ce3f98416c98f18b9f780830e
kly001 <karenly1961@gmail.com>
2020-07-27 22:26:50 -0400

    islands.py from lecture

---
+++ islands.py
@@ -0,0 +1,23 @@
+"""
+
+Write a function that takes a 2D binary array and returns the number of 1 islands. An island consists of 1s that are connected to the north, south, east or west. For example:
+islands = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+island_counter(islands) # returns 4
+
+1. Describe in graph terms:
+    What are the nodes? ==> all the 1's
+    When do we have a edge to another node? ==> If it is one step away in any direction (NSEW)
+
+What do we call a group of nodes? => Connected components
+
+2. 
+
+3. 
+
+
+
+"""
